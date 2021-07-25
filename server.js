d5701880b9849d28c86159177928bbce5dc14206
lalami0908 <b06705048@ntu.edu.tw>
2021-07-25 19:58:59 +0800

    schedule check every morning

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = require('./app');
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require('dotenv-defaults').config();
+const schedule  = require('node-schedule');
+const parser = require('cron-parser');
 
 if(process.env.DBIS_MONGO === 'true' ){
     const { conn } = require('./utils/mongoDB');
@@ -14,9 +16,28 @@ app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const dailySchedule = ()=>{
+    var taskFreq = '1 * * * * *';
+    schedule.scheduleJob(taskFreq,()=>{
+        request('http://localhost:4000/greeting/api/v1', { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        console.log("birthday message: " + "\n" + res.body);
+        });
+    }); 
+}
+
+const checkDailySchedule = ()=>{
+    var taskFreq =  parser.parseExpression('0 0 0 * * *');
+     // check for the cron expression
+     for (let i = 0; i < 5; ++i) {
+        console.log(taskFreq.next().toString());
+    }
+}
+
 const port = process.env.PORT || 4000 
 app.listen(port, () =>{
 	console.log(`Example app listening on port ${port}!`);
+	// dailySchedule();
 });
 
 app.use((err, req, res, next) => {
