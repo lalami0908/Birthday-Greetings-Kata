0f93b0b29e1bc99bbd38f9243b63ea5ab310212d
lalami0908 <b06705048@ntu.edu.tw>
2021-07-25 18:37:55 +0800

    add data when mysql connect

--- utils/mysql.js
+++ utils/mysql.js
@@ -18,7 +18,17 @@ conn.connect(function(err) {
 });
 
 async function initMySQL () {
- 
+await query('TRUNCATE TABLE members');
+  var sqls = [
+  'INSERT INTO members (First_Name, Last_Name, Gender, Date_of_Birth, Email) VALUES ("Robert", "Yen", "Male", "1975/8/8", "robert.yen@linecorp.com")',
+  'INSERT INTO members (First_Name, Last_Name, Gender, Date_of_Birth, Email) VALUES ("Cid", "Change", "Male", "1990/10/10", "cid.change@linecorp.com")',
+  'INSERT INTO members (First_Name, Last_Name, Gender, Date_of_Birth, Email) VALUES ("Miki", "Lai", "Female", "1993/4/5", "miki.lai@linecorp.com")',
+  'INSERT INTO members (First_Name, Last_Name, Gender, Date_of_Birth, Email) VALUES ("Sherry", "Chen", "Female", "1993/8/8", "sherry.lai@linecorp.com")',
+  'INSERT INTO members (First_Name, Last_Name, Gender, Date_of_Birth, Email) VALUES ("Peter", "Wang", "Male", "1950/12/22", "peter.wang@linecorp.com")'];
+  for (var i = 0; i < sqls.length; i++) {
+      await query(sqls[i]);
+  };
+  console.log('mysql adding data completed');
 }
 
 
