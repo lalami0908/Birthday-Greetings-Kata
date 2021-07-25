103c4aade21e44788a7a96a952fb84f33132b625
lalami0908 <b06705048@ntu.edu.tw>
2021-07-25 18:19:39 +0800

    function: getTodayStr and fix overwrite Model Member

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 const express = require("express");
 const app = express();
 
+const Member = require('./models/member')
+
+function getTodayStr (today) {
+    var todayDate = new Date(today);
+    if (!todayDate instanceof Date || isNaN(todayDate.getTime())){
+        todayDate = new Date();
+    }
+    var month = todayDate.getMonth()+1;
+    var date = todayDate.getDate();
+    var todayStr = "/"+ month+ "/"+ date;
+    console.log(`query birthday: ${todayStr}`);
+    return todayStr;
+};
+
+app.get('/greeting/api/v2', async (req, res) => { 
+    let { today } = req.query;
+    let todayStr = getTodayStr(today);
+
+    await Member.find( { Date_of_Birth: { $regex: todayStr, $options: 'i' } }).then((members)=>{
+        return res.json("test");
+       
+    })
+});
+
 module.exports = app;
